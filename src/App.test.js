f791fb11d4877fe43afae8f99b3a692365a41e01
Juan Pablo Cardenas Herrera <jcardenas@ucb.edu.bo>
2021-10-14 09:24:15 -0400

    detecta avance hacia izquierda

--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,17 @@ describe("Sumar", () => {
   it("recibe un caracter e interpreta",()=>{
       expect(interpretarCaracter("A")).toEqual(1);
   });
+  it("recibe un caracter e interpreta",()=>{
+    expect(interpretarCaracter("I")).toEqual(1);
+  });
 });
 
 function interpretarCaracter(caracter){
   let posicion=0;
   if(caracter=="A"){
+    posicion=posicion+1;
+  }
+  if(caracter=="I"){
     posicion=posicion+1;
   }
   return posicion;
