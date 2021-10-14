160f3066a5e4e76aa33bb36272d9cb62b89d692b
Juan Pablo Cardenas Herrera <jcardenas@ucb.edu.bo>
2021-10-14 09:16:13 -0400

    detecta avance hacia adelante

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
 import sumar from "./App.js";
 
 describe("Sumar", () => {
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+  it("recibe un caracter e interpreta",()=>{
+      expect(interpretarCaracter("A")).toEqual(1);
   });
 });
+
+function interpretarCaracter(caracter){
+  let posicion=0;
+  if(caracter=="A"){
+    posicion=posicion+1;
+  }
+  return posicion;
+}
+
+
---
+++ src/Saltar.test.js
@@ -0,0 +1,9 @@
+describe("carrito",()=>{
+    it("recibe un numero y lo retorna",()=>{
+        expect(base()).toEqual(1);
+    });
+});
+
+function base(){
+    return 1;
+}
